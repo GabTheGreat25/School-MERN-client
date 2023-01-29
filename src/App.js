6e3a66b2b90cc3d8d5d92617c2afc7a17acc9ec5
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-01-29 17:46:13 +0800

    add some console and change some codes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import logo from './logo.svg';
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Nav from './Nav';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Nav from "./Nav";
+import axios from "axios";
 
 // function App() {
 //   return (
@@ -28,52 +28,64 @@ import axios from 'axios';
 const App = () => {
   const [posts, setPosts] = useState([]);
   const fetchPosts = () => {
-      axios
-          .get(`${process.env.REACT_APP_API}/posts`)
-          .then(response => {
-              // console.log(response);
-              setPosts(response.data);
-          })
-          .catch(error => alert('Error fetching posts'));
+    axios
+      .get(`${process.env.REACT_APP_API}/posts`)
+      .then((response) => {
+        console.log(response);
+        setPosts(response.data);
+        console.log(axios);
+        console.log("posts", posts);
+        console.log("response.data", response.data[0]);
+        console.log("process.env.REACT_APP_API", process.env.REACT_APP_API);
+        // console.log("response.data.title", response.data.title);
+        // console.log("response.data.content", response.data.content);
+        // console.log("response.data.user", response.data.user);
+        // console.log("response.data.createdAt", response.data.createdAt);
+        // console.log("response.data.updatedAt", response.data.updatedAt);
+        // console.log("response.data._id", response.data._id);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        alert("Error fetching posts");
+      });
   };
 
   useEffect(() => {
-      fetchPosts();
+    fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   //FORM
   return (
     <div className="container pb-5">
-        <Nav />
-        <br />
-        <h1>MERN CRUD</h1>
-        <hr />
-        {posts.map((post, i) => (
-            <div className="row" key={post._id} style={{ borderBottom: '1px solid silver' }}>
-                <div className="col pt-3 pb-2">
-                    <h2>{post.title}</h2>
-                    <p className="lead">{post.content.substring(0, 100)}</p>
-                    <p>
-                        Author <span className="badge">{post.user}</span> Published on{' '}
-                        <span className="badge">{new Date(post.createdAt).toLocaleString()}</span>
-                    </p>
-                </div>
-            </div>
-        ))}
+      <Nav />
+      <br />
+      <h1>MERN CRUD</h1>
+      <hr />
+      {posts.map((post) => (
+        <div
+          className="row"
+          key={post._id}
+          style={{ borderBottom: "1px solid silver" }}
+        >
+          <div>ID: {post._id}</div>
+          <div className="col pt-3 pb-2">
+            <h2>Title: {post.title}</h2>
+            <p className="lead">{post.content.substring(0, 100)}</p>
+            <p>
+              Author: <span className="badge">{post.user}</span> Published on:
+              <span className="badge">
+                {new Date(post.createdAt).toLocaleString()}
+              </span>
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
-);
+  );
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
 
 //OLD FORM
 // const App = () => (
--- src/Create.js
+++ src/Create.js
@@ -1,108 +1,113 @@
-import React, { useState } from 'react';
-import Nav from './Nav';
-import axios from 'axios';
+import React, { useState } from "react";
+import Nav from "./Nav";
+import axios from "axios";
 
 const Create = () => {
+  // state
+  const [state, setState] = useState({
+    title: "",
+    content: "",
+    user: "",
+  });
 
-    // state
-    const [state, setState] = useState({
-        title: '',
-        content: '',
-        user: ''
-    });
+  // destructure values from state
+  const { title, content, user } = state;
 
-    // destructure values from state
-    const { title, content, user } = state;
+  // onchange event handler
+  const handleChange = (name) => (event) => {
+    console.log("name", name, "event", event.target.value);
+    // setState({ ...state, [name]: event.target.value });
+    setState({ title, content, user, [name]: event.target.value });
+  };
+  console.log(state);
+  console.log("title", title);
+  console.log("content", content);
+  console.log("user", user);
+  console.table({ title, content, user });
+  console.log("process.env.REACT_APP_API", process.env.REACT_APP_API);
 
-    // onchange event handler
-    const handleChange = name => event => {
-        console.log('name', name, 'event', event.target.value);
-        setState({ ...state, [name]: event.target.value });
-    };
-    console.log(state);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      //promises
+      .post(`${process.env.REACT_APP_API}/post`, { ...state })
+      //   .post(`${process.env.REACT_APP_API}/post`, { title, content, user })
+      //process
+      .then((response) => {
+        console.log(response);
+        //empty state
+        // setState({ ...state, title: "", content: "", user: "" });
+        // setState({ title: "", content: "", user: "" }); //! iisa lang
+        setState({ ...state });
+        // show sucess alert
+        alert(`Post titled ${response.data.title} is created`);
+      })
+      //error response
+      .catch((error) => {
+        console.log(error.response);
+        alert(error.response.data.error);
+      });
+  };
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        // console.table({ title, content, user });
-        axios
-            //promises
-            .post(`${process.env.REACT_APP_API}/post`, { title, content, user })
-            //process
-            .then(response => {
-                console.log(response);
-                //empty state
-                setState({ ...state, title: '', content: '', user: '' });
-                // show sucess alert
-                alert(`Post titled ${response.data.title} is created`);
-            })
-            //error response
-            .catch(error => {
-                console.log(error.response);
-                alert(error.response.data.error);
-            });
-    };
+  // function handleChange(name) {
+  //     return function(event) {
+  //         setState({ ...state, [name]: event.target.value });
+  //     };
+  // }
 
-    // function handleChange(name) {
-    //     return function(event) {
-    //         setState({ ...state, [name]: event.target.value });
-    //     };
-    // }
-
-
-
-
-//FORM
-    return (
-
-        <div className="container p-5">
-        <Nav/>
-            <h1>CREATE POST</h1>
-            <br />
-            {JSON.stringify(state)}
-            <form  onSubmit={handleSubmit}>
-             <div className="form-group">
-                    <label className="text-muted">Title</label>
-                    <input
-                        onChange={handleChange('title')}
-                        value={title}
-                        type="text"
-                        className="form-control"
-                        placeholder="Post title"
-                        required
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label className="text-muted">Content</label>
-                    <textarea
-                        onChange={handleChange('content')}
-                        value={content}
-                        type="text"
-                        className="form-control"
-                        placeholder="Write something.."
-                        required
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label className="text-muted">User</label>
-                    <input
-                        onChange={handleChange('user')}
-                        value={user}
-                        type="text"
-                        className="form-control"
-                        placeholder="Your name"
-                        required
-                    />
-                </div>
-
-                <div>
-                    <button className="btn btn-primary">Create</button>
-                </div>
-            </form>
+  //FORM
+  return (
+    <div className="container p-5">
+      <Nav />
+      <h1>CREATE POST</h1>
+      <br />
+      {JSON.stringify(state)}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label className="text-muted">Title</label>
+          <input
+            onChange={handleChange("title")}
+            value={title}
+            type="text"
+            className="form-control"
+            placeholder="Post title"
+            required
+          />
         </div>
-    );
-//END FORM
+
+        <div className="form-group">
+          <label className="text-muted">Content</label>
+          <textarea
+            onChange={handleChange("content")}
+            value={content}
+            type="text"
+            className="form-control"
+            placeholder="Write something.."
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">User</label>
+          <input
+            onChange={handleChange("user")}
+            value={user}
+            type="text"
+            className="form-control"
+            placeholder="Your name"
+            required
+          />
+        </div>
+
+        <div>
+          <button className="btn btn-primary" type="submit">
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+  //END FORM
 };
 
 export default Create;
